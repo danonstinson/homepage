e9ce6b9f11e9233673147ed84b352abe41dcd7c4
Daniel Samsonowski <daniel.samsonowski@gmail.com>
2022-08-05 14:36:50 +0200

    js refactored

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-console.log("hello world");
+{
+    const welcome = () => {
+        console.log("hello world");
+    };
 
-let button = document.querySelector(".js-button");
-let poemVerseFine = document.querySelector(".aside__view");
-let inlineText = document.querySelector(".js-button--modificator")
+    const toggleBackground = () => {
+        const poemVerseFine = document.querySelector(".aside__view");
+        const inlineText = document.querySelector(".js-button--modificator")
 
-button.addEventListener("click", () => {
-    poemVerseFine.classList.toggle("aside__view--modificator");
-    if (poemVerseFine.classList.contains("aside__view--modificator")) {
-        inlineText.innerText = "jasny"
-    } else {
-        inlineText.innerText = "czerwony"
-    }
-});
+        poemVerseFine.classList.toggle("aside__view--modificator");
+        if (poemVerseFine.classList.contains("aside__view--modificator")) {
+            inlineText.innerText = "jasny"
+        } else {
+            inlineText.innerText = "czerwony"
+        }
+    };
+
+    welcome();
+
+    const button = document.querySelector(".js-button");
+
+    button.addEventListener("click", toggleBackground);
+}
